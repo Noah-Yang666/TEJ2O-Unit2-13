f34823abd4f5f0261b377ad9bbba2e576ac7fefd
Noah-Yang666 <noah.yang@stu.ocsb.ca>
2026-04-08 17:58:46 -0400

    program uses loops to count down and light up neopixels

--- main.ts
+++ main.ts
@@ -1,8 +1,43 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Noah Yang
+ * Created on: April 2026
+ * This program uses loops to count down
 */
 
-basic.showString('Hello, World!')
+// variables needed
+let loopCounter: number = 4
+let neopixelStrip: neopixel.Strip = null 
+
+// setting up
+basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+// pressing A and running loop
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    loopCounter = 4
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+    neopixelStrip.show()
+    while (loopCounter >= 0) {
+        neopixelStrip.setPixelColor(loopCounter, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.show()
+        basic.showNumber(loopCounter)
+        loopCounter = loopCounter - 1
+        basic.clearScreen()
+        basic.pause(1000)
+    }
+    basic.clearScreen()
+    neopixelStrip.clear()
+    neopixelStrip.show()
+    basic.showIcon(IconNames.Happy)
+})
